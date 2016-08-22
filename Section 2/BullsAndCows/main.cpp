97892ece4e490be4514791ab53735476485d392c
David Mckoy <dmckoy85@gmail.com>
2016-08-21 23:29:16 -0400

    Adding Section 2

---
+++ Section 2/BullsAndCows/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  BullsAndCows
+//
+//  Created by David Mckoy on 2016-08-21.
+//  Copyright © 2016 David Mckoy. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // insert code here...
+    cout << "Welcome to Bulls and Cows\n" << endl;
+    return 0;
+}
